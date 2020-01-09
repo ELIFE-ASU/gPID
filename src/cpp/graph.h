1289503ec720c4d09f7d96714855e51aec212720
Douglas G. Moore <doug@dglmoore.com>
2020-01-09 10:46:38 -0700

    :fire: Make way for Eolas

--- src/cpp/graph.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "pid.h"
-#include <graphviz/cgraph.h>
-
-auto agraph(inform_pid_lattice const *lattice) -> Agraph_t*;
-auto write_graph(Agraph_t *graph, std::string filename) -> void;
