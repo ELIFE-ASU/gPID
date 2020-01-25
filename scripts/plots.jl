f6be0c02e2805de38c7ed0965acef3f4f1fab6b3
Douglas G. Moore <doug@dglmoore.com>
2020-01-25 00:25:27 -0700

    :hammer: Setup plots to support standard directories

--- scripts/plots.jl
+++ scripts/plots.jl
@@ -1,18 +1,25 @@
-using ArgParse, DataFrames, DrWatson, Parameters, Plots, Printf, StatsPlots
+using ArgParse, DrWatson, Parameters, Plots, Printf, StatsPlots
 
 include(srcdir("gpid.jl"))
 
 const s = ArgParseSettings(version="1.0", add_version=true)
 
 @add_arg_table s begin
+    "--verbose", "-v"
+        help = "enable verbose output"
+        action = :store_true
+end
+
+add_arg_group(s, "Input and Output")
+@add_arg_table s begin
     "--input", "-i"
         help = "input file path"
         arg_type = String
-        default = "results"
-    "--outdir", "-o"
-        help = "output directory for plots"
-        arg_type = String
-        default = "gpid"
+        default = ""
+end
+
+add_arg_group(s, "gPID Plot Settings")
+@add_arg_table s begin
     "--legend", "-l"
         help = "enable plot legends"
         action = :store_true
@@ -22,22 +29,22 @@ const s = ArgParseSettings(version="1.0", add_version=true)
         default = "Partial Information (bits)"
 end
 
-@unpack input, outdir, legend, ylabel = parse_args(s)
+@unpack input, legend, ylabel, verbose = parse_args(s)
 
-const inputdir = datadir(input)
-df = collect_results(inputdir; subfolders=true)
-df[!,:simdir] = relpath.(dirname.(df[:,:path]), inputdir)
+df = collect_results(datadir("results", input); subfolders=true)
 
-groups = groupby(df, [:simdir, :algorithm, :target, :sources])
+groups = groupby(df, [:input, :algorithm, :target, :sources])
 for (key, group) in zip(keys(groups), groups)
-    @unpack simdir, algorithm, target = key
+    @unpack input, algorithm, target = key
     sources = join(string.(key.sources), "_")
 
     filename = savename(Dict(:target => target,
                              :algorithm => string(algorithm),
                              :sources => sources), "svg")
-    filepath = projectdir("plots", outdir, simdir, filename)
+    filepath = projectdir("plots", "gpid", dirname(input), filename)
     mkpath(dirname(filepath))
+
+    verbose && @info "Plotting pid for" sim=dirname(input) algorithm target sources=key.sources
 
     Π = v -> payload(v).Π
     title = @sprintf "gPID (sources: %s)" join(string.(key.sources), " ")
@@ -45,7 +52,8 @@ for (key, group) in zip(keys(groups), groups)
         gpidplot(group, Π; title=title, ylabel=ylabel, legend=legend)
         savefig(filepath)
     catch
-        @error "plotting failed with :density mode; falling back to :uniform" target=target sources=key.sources
+        verbose && @error "plotting failed with :density mode; falling back to :uniform"
+        !verbose && @error "plotting failed with :density mode; falling back to :uniform" target=target sources=key.sources
         gpidplot(group, Π; title=title, ylabel=ylabel, legend=legend, mode=:uniform)
         savefig(filepath)
     end
