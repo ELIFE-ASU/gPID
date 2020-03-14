73d603427eb50a87d4155808ddf5f4c65185bf49
Douglas G. Moore <doug@dglmoore.com>
2020-03-14 13:24:41 -0700

    :hammer: Implement BSON to CSV conversion

---
+++ scripts/tocsv.jl
@@ -0,0 +1,44 @@
+using CSV, DrWatson, DataFrames, Discretizers, Eolas, Parameters
+
+const indir = datadir("results", "2020-01-05")
+const outdir = datadir("results", "2020-01-05", "csv")
+
+function main()
+    if isdir(outdir)
+        rm(outdir; force=true, recursive=true)
+    end
+    mkpath(outdir)
+    for scenario in filter(!=("csv"), readdir(indir))
+        data = collect_results(joinpath(indir, scenario))
+        for group in groupby(data, :sources)
+            outfile = joinpath(outdir, join(string.(group.sources[1]), "_") * ".csv")
+            df = if isfile(outfile)
+                CSV.File(outfile) |> DataFrame
+            else
+                lattice = group.lattice[1]
+                df = DataFrame(replicate=String[], scenario=String[], payload=String[])
+                for name in Eolas.prettyname.(vertices(lattice))
+                    df[!, Symbol(name)] = Float64[]
+                end
+                df
+            end
+            for (s, p) in [(:Iₘᵢₙ, "Imin"), (:Π, "Pi")]
+                for row in eachrow(group)
+                    replicate, _ = splitext(row[:input])
+                    newrow = Dict{Symbol, Any}(:replicate => replicate,
+                                               :scenario => scenario,
+                                               :payload => p)
+                    for vertex in vertices(row[:lattice])
+                        name = Symbol(Eolas.prettyname(vertex))
+                        data = getproperty(payload(vertex), s)
+                        newrow[name] = data
+                    end
+                    @assert length(newrow) == length(row[:lattice]) + 3
+                    push!(df, newrow)
+                end
+            end
+            sort!(df, [:payload, :replicate, :scenario])
+            CSV.write(outfile, df)
+        end
+    end
+end
