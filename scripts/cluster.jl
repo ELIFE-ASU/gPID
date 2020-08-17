1e39600ab5bf54222ab2423b1a0574f02182dc60
Douglas G. Moore <doug@dglmoore.com>
2020-08-17 16:03:46 -0700

    Add clustering analysis

--- scripts/cluster.jl
+++ scripts/cluster.jl
@@ -1,34 +1,133 @@
-using Clustering, DrWatson, DataFrames, Discretizers, Eolas, Parameters
+using CSV, DataFrames, Dates, Clustering, StatsPlots, Printf, DrWatson, StatsBase
 
-function getdata(dir)
-    df = collect_results(datadir("results", "2020-01-05", dir))
-    filter!(r -> length(r.sources) == 4, df)
-    sort!(df, (:sources, :input))
-    data = Array{Float64}(undef, length(df.lattice[1].vertices), size(df, 1))
-    for (i, row) in enumerate(eachrow(df))
-        data[:, i] = map(v -> payload(v).Π, row.lattice.vertices)
-    end
-    df.sources, data
+iscsv(f) = occursin(r"\.csv$", f)
+
+function loaddata()
+	filepaths = filter(iscsv, readdir(datadir("results", "csv"); join=true))
+	
+	dfs = filepaths .|> CSV.File .|> DataFrame
+	
+	foreach(dfs) do df
+		filter!(r -> r.payload == "Pi", df)
+		select!(df, Not([:nsamp, :sampn, :gen, :mrcod, :pop2, :gfp2]))
+		rename!(df, Symbol("ps-recom") => :psrecom, :pop1 => :pop, :gfp1 => :gf, :mrnc => :μ)
+	end
+
+	dfs
 end
 
-function main()
-    sources, dsdata = getdata("ds")
-    data = hcat(dsdata, last(getdata("ps")), last(getdata("ns")), last(getdata("ps-ds")))
-    @unpack assignments = kmeans(data, 4)
+sources(df) = sort(collect(Set(vcat(split.(replace.(string.(names(df)[8:end]), r"[:,{}\[\]]" => ""))...))))
 
-    sources, data, assignments
-    #  stride = size(data, 2) ÷ 4
-    #  ds = assignments[1:stride]
-    #  ps = assignments[1+stride:2stride]
-    #  ns = assignments[1+2stride:3stride]
-    #  psds = assignments[1+3stride:4stride]
+function prepare(dfs)
+	data = nothing
+	column_info = Vector{Any}[]
+	param_order = [:date, :psrecom, :pop, :gf, :μ, :payload]
+	for (i, df) in enumerate(dfs)
+		allsources = "[" * join(sources(df), ", ") * "]"
+		for group in groupby(df, param_order)
+			sorted_group = sort(group, :sampgen)
+			payloads = select(sorted_group, Not([:sampgen; param_order]))
+			for name in names(payloads)
+				push!(column_info, [Array(group[1, param_order]); allsources; String(name)])
+			end
+			new_columns = Array{Float64}(payloads)
+			new_columns .-= transpose(new_columns[1,:])
+			data = isnothing(data) ? new_columns : hcat(data, new_columns)
+		end
+	end
+	data, column_info, param_order
+end
 
-    #  consistent = Int[]
-    #  for i in 1:stride
-    #      if length(Set([ds[i], ps[i], ns[i], psds[i]])) == 4
-    #          push!(consistent, i)
-    #          println(sources[i])
-    #      end
-    #  end
-    #  println(length(consistent))
+function cluster(data, k=2, epsilon = 0.01)
+	clusters = kmeans(data, k)
+	while maximum(clusters.costs) > epsilon
+		clusters = kmeans(data, k += 1)
+	end
+	clusters
+end
+
+function saveclusters(data, column_info, param_order, clusters)
+	today = Dates.format(now(), "Y-mm-ddTHHMMSS")
+	if !isdir(plotsdir("classes", today))
+		mkpath(plotsdir("classes", today))
+	end
+	a, b = extrema(data)
+	subplots = []
+	for i in 1:length(clusters.counts)
+		cluster = data[:, clusters.assignments .== i]
+		lower_bound = clusters.centers[:, i] - minimum(cluster, dims=2)
+		upper_bound = maximum(cluster, dims=2) - clusters.centers[:, i]
+		p = plot(clusters.centers[:, i],
+		         ribbon=(lower_bound, upper_bound),
+		         linewidth=3, color=:gray, legend=false,
+		         xlim=(1, size(cluster, 1)), ylim=(a - 0.1, b + 0.1),
+		         title="Class $i ($(size(cluster, 2)) Members)", ylabel="ΔPi")
+		savefig(p, plotsdir("classes", today, (@sprintf "class_%02d.png" i)))
+		push!(subplots, p)
+	end
+
+	p = plot(subplots..., size=(4200, 3600))
+	savefig(p, plotsdir("classes", today, "clusters.png"))
+
+	if !isdir(datadir("results", "classes", today))
+		mkpath(datadir("results", "classes", today))
+	end
+	for i in 1:length(clusters.counts)
+		members = column_info[clusters.assignments .== 1]
+		df = DataFrame([
+			Date;
+			fill(Float64, length(param_order) - 2);
+			String;
+			String;
+			String
+		], [
+			param_order;
+			:allsources;
+			:node
+		])
+		foreach(member -> push!(df, member), members)
+		CSV.write(datadir("results", "classes", today, (@sprintf "class_%02d.csv" i)), df)
+	end
+end
+
+function optimallycluster(data; k=2, epsilon=0.05, n=100)
+	clusters = kmeans(data, k)
+	totalcost = clusters.totalcost
+	maxcost = maximum(clusters.costs)
+
+	while maxcost > epsilon
+		for i in 1:n
+			new_clusters = kmeans(data, k)
+			new_totalcost = new_clusters.totalcost
+			new_maxcost = maximum(new_clusters.costs)
+
+			if totalcost > new_totalcost || (totalcost == new_totalcost && maxcost > new_maxcost)
+				clusters = new_clusters
+				totalcost = new_totalcost
+				maxcost = new_maxcost
+			end
+		end
+		k += 1
+	end
+	clusters
+end
+
+function runall()
+	@info "Loading data"
+	dfs = loaddata()
+
+	@info "Constructing matrix"
+	data = prepare(dfs)
+
+	@info "Identifying fine-scale clusters"
+	clusters = cluster(data[1])
+
+	@info "Saving plots and CSVs"
+	saveclusters(data..., clusters)
+
+	@info "Identifying coarse-scale clusters"
+	clusters = optimallycluster(data[1])
+
+	@info "Saving plots and CSVs"
+	saveclusters(data..., clusters)
 end
