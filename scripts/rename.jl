166f3d85e6c322df6d3208b5fb18bc418b31a2cd
Douglas G. Moore <doug@dglmoore.com>
2020-08-19 13:51:40 -0700

    Rename parameter keys during file renaming

--- scripts/rename.jl
+++ scripts/rename.jl
@@ -1,8 +1,34 @@
 using Dates, DrWatson
 
+function takekeys(d, keys)
+    ndict = Dict{Symbol,Any}()
+    for k in keys
+        from, to = if k isa Pair
+            k
+        elseif k isa AbstractString
+            k, Symbol(k)
+        else
+            String(k), k
+        end
+        ndict[to] = d[from]
+    end
+    ndict
+end
+takekeys(d, keys...) = takekeys(d, keys)
+
 foreach(readdir(datadir("sims"); join=true)) do from
     if occursin(r"\.csv$", from)
         _, params, _ = parse_savename(from; parsetypes=(Int, Float64, Date))
+        params = takekeys(params,
+            :date,
+            "ps-recom" => :psrecom,
+            "gfp1" => :gf,
+            "mrnc" => :μ,
+            "pop1" => :pop,
+            :sampgen,
+            :gen,
+            :nsamp
+        )
         base = savename(params, "csv";
             allowedtypes= (Real, String, Symbol, Date),
             digits = 3,
