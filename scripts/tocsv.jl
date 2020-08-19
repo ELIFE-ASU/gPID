2e405eb228ccad5f4513c92de3fe617472374af3
Douglas G. Moore <doug@dglmoore.com>
2020-08-19 15:45:05 -0700

    Update script to construct CSV files

--- scripts/tocsv.jl
+++ scripts/tocsv.jl
@@ -1,44 +1,53 @@
 using CSV, DrWatson, DataFrames, Discretizers, Imogen, Parameters
 
-const indir = datadir("results", "2020-01-05")
-const outdir = datadir("results", "2020-01-05", "csv")
+include(srcdir("bin.jl"))
 
-function main()
+function tocsv(indir)
+    outdir = joinpath(indir, "csv")
     if isdir(outdir)
         rm(outdir; force=true, recursive=true)
     end
     mkpath(outdir)
-    for scenario in filter(!=("csv"), readdir(indir))
-        data = collect_results(joinpath(indir, scenario))
-        for group in groupby(data, :sources)
-            outfile = joinpath(outdir, join(string.(group.sources[1]), "_") * ".csv")
-            df = if isfile(outfile)
-                CSV.File(outfile) |> DataFrame
-            else
-                lattice = group.lattice[1]
-                df = DataFrame(replicate=String[], scenario=String[], payload=String[])
-                for name in Imogen.prettyname.(vertices(lattice))
-                    df[!, Symbol(name)] = Float64[]
-                end
-                df
-            end
-            for (s, p) in [(:Iₘᵢₙ, "Imin"), (:Π, "Pi")]
-                for row in eachrow(group)
-                    replicate, _ = splitext(row[:input])
-                    newrow = Dict{Symbol, Any}(:replicate => replicate,
-                                               :scenario => scenario,
-                                               :payload => p)
-                    for vertex in vertices(row[:lattice])
-                        name = Symbol(Imogen.prettyname(vertex))
-                        data = getproperty(payload(vertex), s)
-                        newrow[name] = data
-                    end
-                    @assert length(newrow) == length(row[:lattice]) + 3
-                    push!(df, newrow)
+    data = collect_results(indir)
+    for group in groupby(data, :sources)
+        outfile = joinpath(outdir, join(string.(group.sources[1]), "_") * ".csv")
+        sorted_columns = Symbol[]
+        df = if isfile(outfile)
+            CSV.File(outfile) |> DataFrame
+        else
+            lattice = group.lattice[1]
+            input = group.input[1]
+            df = DataFrame()
+            for (k, v) in parse_savename(input)[2]
+                column_name = Symbol(k)
+                push!(sorted_columns, column_name)
+                if v isa Real
+                    df[!, column_name] = Float64[]
+                else
+                    df[!, column_name] = typeof(v)[]
                 end
             end
-            sort!(df, [:payload, :replicate, :scenario])
-            CSV.write(outfile, df)
+            df[!, :payload] = String[]
+            for name in Imogen.prettyname.(vertices(lattice))
+                df[!, Symbol(name)] = Float64[]
+            end
+            df
         end
+        for (s, p) in [(:Iₘᵢₙ, "Imin"), (:Π, "Pi")]
+            for row in eachrow(group)
+                newrow = Dict{Symbol, Any}(:payload => p)
+                for (k, v) in parse_savename(row[:input])[2]
+                    newrow[Symbol(k)] = v
+                end
+                for vertex in vertices(row[:lattice])
+                    name = Symbol(Imogen.prettyname(vertex))
+                    data = getproperty(payload(vertex), s)
+                    newrow[name] = data
+                end
+                push!(df, newrow)
+            end
+        end
+        sort!(df, sorted_columns)
+        CSV.write(outfile, df)
     end
 end
