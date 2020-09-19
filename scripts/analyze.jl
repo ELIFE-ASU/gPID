db59461407965dbd487cec389a48555b904ef023
Douglas G. Moore <doug@dglmoore.com>
2020-09-18 20:10:48 -0700

    Move outputs to generation-specific directories

--- scripts/analyze.jl
+++ scripts/analyze.jl
@@ -6,10 +6,18 @@ end
 
 function runall()
 	dir = datadir("sims")
-	filenames = filter(s -> occursin(r"\.csv$", s), readdir(dir))
+    filenames = filter(s -> occursin(r"\.csv$", s), readdir(dir))
     @threads :static for filename in filenames
         gpid(filename)
 	end
+
+    bsons = filter(s -> occursin(r"\.bson$", s), readdir(datadir("results"); join=true))
+    params = map(p -> p[2], parse_savename.(bsons))
+    for (param, bson) in zip(params, bsons)
+        dir = datadir("results", "gen=$(param["gen"])")
+        mkpath(dir)
+        mv(bson, joinpath(dir, basename(bson)))
+    end
 end
 
 runall()
